38146ef7e9b47139d3a9b25a7933bead98b8a36d
hudsonfs7 <99773919+hudsonfs7@users.noreply.github.com>
2022-04-26 11:13:08 -0300

    Update sair.js

--- js/sair.js
+++ js/sair.js
@@ -4,11 +4,11 @@ textUse.innerText = `Bem-vindo, ${userLogado.nome}!`
 
 if (localStorage.getItem('token') == null) {
   alert('Você precisa estar logado para acessar esta página')
-  window.location.href = 'http://127.0.0.1:5500/'
+  window.location.href = './index.html'
 }
 
 function sair() {
   localStorage.removeItem('token')
   localStorage.removeItem('userLogado')
-  window.location.href = 'http://127.0.0.1:5500/'
+  window.location.href = './index.html'
 }
